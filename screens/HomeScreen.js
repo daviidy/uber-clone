dad904eccf2cb3a072baa66e76ca2d2e7d2d6448
daviidy <yaodavidarmel@gmail.com>
2022-03-01 10:14:40 -0600

    added google autocomplete

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { StyleSheet, SafeAreaView, View, Image } from 'react-native'
 import tw from 'tailwind-react-native-classnames'
 import NavOptions from '../components/NavOptions'
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import { GOOGLE_MAPS_APIKEY } from '@env'
+import { useDispatch } from 'react-redux'
+import { setDestination, setOrigin } from '../slices/navSlice'
 
 const HomeScreen = () => {
+  const dispatch = useDispatch()
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View>
@@ -17,7 +22,40 @@ const HomeScreen = () => {
             uri: 'https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png'
           }}
         />
-        <NavOptions/>
+
+        <GooglePlacesAutocomplete
+          placeholder='Where from?'
+          onPress={(data, details = null) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description
+              })
+            )
+
+            dispatch(setDestination(null))
+          }}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: 'en'
+          }}
+          minLength={2}
+          fetchDetails
+          returnKeyType='search'
+          enablePoweredByContainer={false}
+          styles={{
+            container: {
+              flex: 0
+            },
+            textInput: {
+              fontSize: 18
+            }
+          }}
+          nearbyPlacesAPI='GooglePlacesSearch'
+          debounce={400}
+        />
+
+        <NavOptions />
       </View>
     </SafeAreaView>
   )
